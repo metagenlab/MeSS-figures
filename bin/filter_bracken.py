9591012af17437235943fe2c5701adb169b28504
farchaab <farid.chaabane@chuv.ch>
2024-08-14 10:17:39 +0200

    added abundance sort

--- bin/filter_bracken.py
+++ bin/filter_bracken.py
@@ -13,7 +13,7 @@ subset_df = df[df.new_est_reads >= nb][["taxonomy_id", "new_est_reads"]]
 subset_df["seq_abundance"] = [
     reads / subset_df.new_est_reads.sum() for reads in subset_df.new_est_reads
 ]
-subset_df.sort_values("new_est_reads", ascending=False)
+subset_df.sort_values("new_est_reads", ascending=False, inplace=True)
 subset_df.columns = ["taxon", "reads", "seq_abundance"]
 subset_df.to_csv(
     output,
