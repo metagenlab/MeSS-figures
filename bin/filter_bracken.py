2a0034bfec3763afe9675fbd59b7fec2238ca2fe
farchaab <farid.chaabane@chuv.ch>
2024-08-26 16:03:37 +0200

    output reads by default

--- bin/filter_bracken.py
+++ bin/filter_bracken.py
@@ -10,11 +10,8 @@ nb = int(sys.argv[1])
 df = pd.read_csv(sys.argv[2], sep="\t")
 output = sys.argv[3]
 subset_df = df[df.new_est_reads >= nb][["taxonomy_id", "new_est_reads"]]
-subset_df["seq_abundance"] = [
-    reads / subset_df.new_est_reads.sum() for reads in subset_df.new_est_reads
-]
 subset_df.sort_values("new_est_reads", ascending=False, inplace=True)
-subset_df.columns = ["taxon", "reads", "seq_abundance"]
+subset_df.columns = ["taxon", "reads"]
 subset_df.to_csv(
     output,
     sep="\t",
